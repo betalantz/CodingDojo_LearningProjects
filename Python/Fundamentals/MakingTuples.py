0cf58a58da4f7ddfe88084d342e2bf3935936390
betalantz <betalantz@gmail.com>
2017-08-01 23:33:31 -0700

    added MakingTuples to Fundamentals

---
+++ Python/Fundamentals/MakingTuples.py
@@ -0,0 +1,10 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+def dictToTuples(dict):
+    print(dict.items())
+
+dictToTuples(my_dict)
---
+++ Python/Fundamentals/names_solution.py
@@ -0,0 +1,37 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def show_students(arr):
+    for student in students:
+        print student['first_name'], student['last_name']
+
+def show_all(users):
+    for role in users:
+        counter = 0
+        print role
+        for person in users[role]:
+            counter += 1
+            first_name = person['first_name'].upper()
+            last_name = person['last_name'].upper()
+            length = len(first_name) + len(last_name)
+            print "{} - {} {} - {}".format(counter, first_name, last_name, length)
+
+show_students(students)
+show_all(users)
