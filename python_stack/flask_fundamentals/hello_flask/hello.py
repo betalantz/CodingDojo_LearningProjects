d3558b1cb1790cae95a729c5aa79c77eaa10af19
betalantz <betalantz@gmail.com>
2017-08-04 13:10:23 -0700

    first Flask examples

--- python_stack/flask_fundamentals/hello_flask/hello.py
+++ python_stack/flask_fundamentals/hello_flask/hello.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
-    return 'Hello world!'
+    return render_template('index.html')
+
+@app.route('/success')
+
+def success():
+    return render_template('success.html')
+
+
 app.run(debug=True)
