cec07df47b277504383af05e0c7796d9af2badde
betalantz <betalantz@gmail.com>
2017-08-04 20:19:36 -0700

    a server doc that can handle 3 route reqs

---
+++ python_stack/flask_fundamentals/portfolio/portfolio.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+app.run(debug=True)
