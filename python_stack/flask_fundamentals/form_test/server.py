e80f2507438e8e4aa7f288dcb73a5dbdfe05ee3b
betalantz <betalantz@gmail.com>
2017-08-06 19:16:02 -0700

    added session to this test proj

--- python_stack/flask_fundamentals/form_test/server.py
+++ python_stack/flask_fundamentals/form_test/server.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-
+app.secret_key = 'trifecta'
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/users', methods=['POST'])
 def create_user():
     print "Got POST info"
-    name = request.form['name']
-    email = request.form['email']
-    print name, email
-    return redirect('/')
+    session['name'] = request.form['name']
+    session['email'] = request.form['email']
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+  return render_template('user.html')
+
 app.run(debug=True)
