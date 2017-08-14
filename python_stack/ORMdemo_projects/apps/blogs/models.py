f8a7885627d72f1fc84511ab7dab318098460322
betalantz <betalantz@gmail.com>
2017-08-13 21:51:04 -0700

    first exploration of models and ORM

---
+++ python_stack/ORMdemo_projects/apps/blogs/models.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# Inside models.py
+class Blog(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __repr__(self):
+        return "<Blog object: {} {}>".format(self.name, self.desc)
+class Comment(models.Model):
+    comment = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __repr__(self):
+        return "<Comment object: {} {}>".format(self.name, self.desc)
+    # Notice the association made with ForeignKey for a one-to-many relationship
+    # There can be many comments to one blog
+    blog = models.ForeignKey(Blog, related_name = "comments")
+class Admin(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    blogs = models.ManyToManyField(Blog, related_name = "admins")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __repr__(self):
+        return "<Admin object: {} {}>".format(self.name, self.desc)
