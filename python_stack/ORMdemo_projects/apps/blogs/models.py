70b02cd608b047fe682303d36a7405be87f45f69
betalantz <betalantz@gmail.com>
2017-08-14 16:46:57 -0700

    added BlogManager method for basic validation

--- python_stack/ORMdemo_projects/apps/blogs/models.py
+++ python_stack/ORMdemo_projects/apps/blogs/models.py
@@ -3,12 +3,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-# Inside models.py
+#Our new manager!
+#No methods in our new manager should ever catch the whole request object with a parameter!!! (just parts, like request.POST)
+class BlogManager(models.Magager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['name'])<5:
+            errors["name"]="Blog name should be more than 5 characters."
+        if len(postData['desc'])<10:
+            errors["desc"]="Blog desc should be more than 10 characters."
+        return errors;
+
 class Blog(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+    # *************************
+    # Connect an instance of BlogManager to our User model overwriting
+    # the old hidden objects key with a new one with extra properties!!!
+    objects = BlogManager()
+    # *************************
     def __repr__(self):
         return "<Blog object: {} {}>".format(self.name, self.desc)
 class Comment(models.Model):
--- python_stack/ORMdemo_projects/apps/blogs/views.py
+++ python_stack/ORMdemo_projects/apps/blogs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User
 
-# Create your views here.
+def update(request):
+    errors = Blog.objects.basic_validator(request.POST)
+        if len(errors):
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('/blog/edit/'+id)
+        else:
+            blog = Blog.objects.get(id = id)
+            blog.name = request.POST['name']
+            blog.desc = request.POST['desc']
+            blog.save()
+            return redirect('/blogs')
