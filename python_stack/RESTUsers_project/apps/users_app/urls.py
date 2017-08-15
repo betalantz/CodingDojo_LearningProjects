d0b97f9f599a4ddceb42ad0b910188a53963b5a0
betalantz <betalantz@gmail.com>
2017-08-15 14:44:10 -0700

    Index, Show, and Edit pages functional

--- python_stack/RESTUsers_project/apps/users_app/urls.py
+++ python_stack/RESTUsers_project/apps/users_app/urls.py
@@ -5,5 +5,12 @@ def test(request):
     print 'welcome to users_app urls'
 
 urlpatterns = [
-    url(r'^', views.index),
+    url(r'^$', views.index),
+    url(r'^users/$', views.index, name='my_index'),
+    url(r'^users/new', views.new, name='my_new'),
+    url(r'^users/create', views.create, name='my_create'),
+    url(r'^users/(?P<id>\d+)/$', views.show, name='my_show'),
+    url(r'^users/(?P<id>\d+)/edit/$', views.edit, name='my_edit'),
+    url(r'^users/(?P<id>\d+)/destroy/$', views.destroy, name='my_destroy'),
+
 ]
--- python_stack/RESTUsers_project/apps/users_app/views.py
+++ python_stack/RESTUsers_project/apps/users_app/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from models import *
 
 def test(request):
     print "# Create your views here."
 
 def index(request):
-    return render(request, "users_app/index.html")
+    return render(request, "users_app/index.html", {"users_list": User.objects.all()})
+
+def new(request):
+    response = 'placeholder to later display a new form to create a new user'
+    return HttpResponse(response)
+
+def create(request):
+    return redirect('/users/')
+
+def show(request, id):
+    if request.method=='POST':
+        print 'hello im in show'
+        user = User.objects.get(id = id)
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+    return render(request, "users_app/show.html", {"user": User.objects.get(id=id)})
+
+
+def edit(request, id):
+    return render(request, "users_app/edit.html", {"user": User.objects.get(id=id)}) 
+
+# def update(request, id):
+#     return redirect('/users')
+
+def destroy(request, id):
+    return redirect('/users')
