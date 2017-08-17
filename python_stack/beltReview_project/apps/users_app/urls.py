dfabd86966b0c5d164cc35c5e0da4818b9763912
betalantz <betalantz@gmail.com>
2017-08-17 14:12:28 -0700

    set up structure and login/registration

---
+++ python_stack/beltReview_project/apps/users_app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+# def test(request):
+#     print 'welcome to logins_app urls'
+
+urlpatterns = [
+    # url(r'^', test),
+    url(r'^$', views.index),
+    url(r'^register/$', views.register, name='my_register'),
+    url(r'^login/$', views.login, name='my_login'),
+    url(r'^success/$', views.success, name='my_success'),
+    url(r'^logout/$', views.logout, name='my_logout'),
+]
---
+++ python_stack/beltReview_project/apps/users_app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, HttpResponse, redirect
+from models import *
+from django.contrib import messages
+
+# def test(request):
+#     print "/\/\/\/\/\/ Create your views here."
+
+def index(request):
+    return render(request, "users_app/index.html") 
+
+def register(request):
+    results = User.objects.register_validator(request.POST)
+    if results['status']==False:
+        for error in results['errors']:
+            messages.error(request, error)
+        return redirect('/index')
+    user = User.objects.createUser(request.POST)
+    messages.success(request, 'You registered successfully! Please log in.')
+    return redirect('/')
+
+def login(request):
+    results = User.objects.login_validator(request.POST)
+    if results['status']==False:
+        for error in results['errors']:
+            messages.error(request, error)
+        return redirect('/index')
+    # store a user auth token in sessions to allow a session check
+    request.session['user_id']=results['user'].id
+    request.session['user_first_name']=results['user'].first_name
+
+    storage = messages.get_messages(request)
+    storage.used = True
+    return redirect('/success')
+
+def sessionCheck(request):
+    try:
+        return request.session['user_id']
+    except:
+        return False
+
+def success(request):
+    if sessionCheck(request)==False:
+        return redirect ('/')
+    messages.success(request, 'Successfully logged in!')
+    # return render(request, 'users_app/success.html')
+    return HttpResponse(self, 'This will link to HOME page.')
+
+def logout(request):
+    request.session.flush()
+    return redirect ('/')
