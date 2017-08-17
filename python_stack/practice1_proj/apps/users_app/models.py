91273ca5d0a5b8354335401dc148fbd8c4845877
betalantz <betalantz@gmail.com>
2017-08-17 16:21:28 -0700

    added class models, but no manager yet

--- python_stack/practice1_proj/apps/users_app/models.py
+++ python_stack/practice1_proj/apps/users_app/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+        # objects = UserManager()
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(User, related_name = 'comments')
