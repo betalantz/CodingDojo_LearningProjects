ee91cf69a493fa8d9d46a8bfeebb24156776ad45
betalantz <betalantz@gmail.com>
2017-08-18 12:13:10 -0700

    connected game

--- python_stack/goldLeader_project/apps/game_app/urls.py
+++ python_stack/goldLeader_project/apps/game_app/urls.py
@@ -5,5 +5,7 @@ def test(request):
     print "[[[[[[[[  game urls ]]]]]]]]]"
 
 urlpatterns = [
-    url(r'^game/$', views.test),
+    url(r'^game/$', views.game, name='my_game'),
+    url(r'^game/process$', views.process),
+
 ]
