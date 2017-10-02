f7d3a2218daee2cf0e215eb6af819d42d93f1283
betalantz <betalantz@gmail.com>
2017-10-02 16:13:54 -0700

    How to iterate JS objects with for-in

---
+++ MEAN/JS Fundamentals/Obj-demo/obj.js
@@ -0,0 +1,14 @@
+// here is an object literal with four key-value pairs
+let sandwich = {
+    cheese: 'Smoked Gouda',
+    meat: 'Dry-aged Bison',
+    sauce: 'Chipotle Aioli',
+    veggies: 'Caramelized Onions'
+}
+// the variable 'topping' is used instead of an index
+for(let topping in sandwich){
+    // when we log 'topping', we notice it's a key
+    console.log(topping);
+    // when we pass the key to the 'sandwich' object, we can pull values
+    console.log(sandwich[topping]);
+}
