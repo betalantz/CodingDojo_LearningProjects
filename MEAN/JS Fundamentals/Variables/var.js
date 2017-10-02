6c4347b25c9cafb4607d6de90495fc51f21736ae
betalantz <betalantz@gmail.com>
2017-10-02 11:20:40 -0700

    demo code from Varibles tab

---
+++ MEAN/JS Fundamentals/Variables/var.js
@@ -0,0 +1,20 @@
+var x = 10;
+console.log(x);
+var myName = 'Kadie';
+var myAge = 29;
+var myBirthday;
+console.log('Hello World!');
+// => Hello World!
+console.log(myName);
+// => Kadie
+console.log(myAge);
+// => 29
+console.log(myBirthday);
+// => undefined
+var x = [1, 2, 3];
+var y = x;
+x.push('Hello world!')
+// push adds to the end of arrays in JavaScript
+console.log(x);
+console.log(y);
+console.log('\u263A        \u2603        \u272f');
