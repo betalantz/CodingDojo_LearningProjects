89692aa930ebe745fcd5de8b67368eda0a54a970
betalantz <betalantz@gmail.com>
2017-10-05 10:54:23 -0700

    built out with some ejs

--- MEAN/Express/express_demo/server.js
+++ MEAN/Express/express_demo/server.js
@@ -8,6 +8,13 @@ const PORT = 8000 //this setting optional
 app.set("views", path.join(__dirname, "./client/views")) //handles diff path separators from diff platorms/OS
 app.set("view engine", "ejs")
 
+app.get("/", (request, response) => {
+    console.log("GET request at '/'");
+    let my_name = "Jack"
+    let cool_numbers = [13, 3, 42, 22/7, 227]
+    response.render("index", {name: my_name, nums: cool_numbers})
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
