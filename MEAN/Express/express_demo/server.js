6e02c5fe5abc5f91846aa8b7c4d82b1b21b77711
betalantz <betalantz@gmail.com>
2017-10-05 12:37:59 -0700

    added users example

--- MEAN/Express/express_demo/server.js
+++ MEAN/Express/express_demo/server.js
@@ -14,6 +14,16 @@ app.get("/", (request, response) => {
     let cool_numbers = [13, 3, 42, 22/7, 227]
     response.render("index", {name: my_name, nums: cool_numbers})
 })
+app.get("/users", function (request, response){
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
