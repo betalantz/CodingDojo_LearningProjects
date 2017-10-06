aa2ba484855c9431a2f0825d5851926ffac76085
betalantz <betalantz@gmail.com>
2017-10-06 14:25:47 -0700

    added sockets to Survey Form assignment

--- MEAN/Express/SocketsIO/survey_revisited/server.js
+++ MEAN/Express/SocketsIO/survey_revisited/server.js
@@ -12,21 +12,10 @@ app.use(express.static(path.join(__dirname, "./node_modules")))
 app.set('views', path.join(__dirname, './client/views'))
 app.set('view engine', 'ejs')
 
-// app.get('/', function(req, res) {
-//     res.render("index")
-// })
+app.get('/', function(req, res) {
+    res.render("index")
+})
 
-// app.post('/result', function(req, res) {
-//     console.log("POST DATA", req.body);
-//     let result = {
-//         name: req.body.name,
-//         location: req.body.location,
-//         language: req.body.language,
-//         comment: req.body.comment
-//     }
-//     res.render('results', {result: result})
-//     // res.redirect('/')
-// })
 
 const server = app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`);
@@ -35,4 +24,11 @@ const io = require("socket.io").listen(server)
 
 io.sockets.on("connection", socket => {
     console.log("New connection", socket.id);
+
+    socket.on("posting_form", (newData) => {
+        console.log(newData);
+        let lucky = Math.floor(Math.random() * 1000) + 1
+        io.emit("updated_message", newData)
+        io.emit("random_number", lucky)
+    })
 })
