80a393dbfa41d9c0882f8bde69d579e93189db24
betalantz <betalantz@gmail.com>
2017-10-09 17:10:50 -0700

    can Create and Read with MongoDB

--- MEAN/MongoDB/Intro Mongoose/basic_mongoose_app/server.js
+++ MEAN/MongoDB/Intro Mongoose/basic_mongoose_app/server.js
@@ -19,24 +19,48 @@ app.set('views', path.join(__dirname, './client/views'));
 // Setting our View Engine set to EJS
 app.set('view engine', 'ejs');
 // Routes
-// Root Request
+// The root route -- we want to get all of the users from the database and then render the index view passing it all of the users
 app.get('/', function(req, res) {
-    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
-    res.render('index');
-})
+    User.find({}, function(err, users) {
+      // This is the method that finds all of the users from the database
+      // Notice how the first parameter is the options for what to find and the second is the
+      //   callback function that has an error (if any) and all of the users
+      // Keep in mind that everything you want to do AFTER you get the users from the database must
+      //   happen inside of this callback for it to be synchronous 
+      // Make sure you handle the case when there is an error, as well as the case when there is no error
+      if(err){
+          console.log("You done messed up, son!");
+      } else {
+          console.log("Sending users to index.ejs")
+          res.render('index', {users})
+      }
+    })
+  })
+  
 // Add User Request 
 app.post('/users', function(req, res) {
     console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    res.redirect('/');
-})
+    // create a new User with the name and age corresponding to those from req.body
+    var user = new User({name: req.body.name, age: req.body.age});
+    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+    user.save(function(err) {
+      // if there is an error console.log that something went wrong!
+      if(err) {
+        console.log('something went wrong');
+      } else { // else console.log that we did well and then redirect to the root route
+        console.log('successfully added a user!');
+        res.redirect('/');
+      }
+    })
+  })
+  
 // Setting our Server to Listen on Port: 8000
 app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`);
 })
 // This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
 //   our db in mongodb -- this should match the name of the db you are going to use for your project.
-mongoose.connect('mongodb://localhost/basic_mongoose_app');
+mongoose.connect('mongodb://localhost/basic_mongoose_app', {useMongoClient: true});
 
 var UserSchema = new mongoose.Schema({
     name: String,
@@ -45,3 +69,5 @@ var UserSchema = new mongoose.Schema({
    mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
    var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
    
+// Use native promises
+mongoose.Promise = global.Promise;
