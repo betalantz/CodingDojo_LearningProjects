f15ee82020a66725f7aa3b106ccb83998f341a17
betalantz <betalantz@gmail.com>
2017-10-12 13:43:32 -0700

    added attribute directives (styling)

--- MEAN/Angular/myFirstAngular/myFirstAngularApp/src/app/app.component.ts
+++ MEAN/Angular/myFirstAngular/myFirstAngularApp/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color = 'red';
   title = 'No Demons Here';
+  switch: boolean = true;
 }
