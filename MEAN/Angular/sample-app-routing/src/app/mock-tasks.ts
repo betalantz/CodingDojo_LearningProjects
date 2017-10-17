0590b2f8e6c74d38e52c7069f1fc58740ae1abc3
betalantz <betalantz@gmail.com>
2017-10-17 12:11:27 -0700

    first commit

---
+++ MEAN/Angular/sample-app-routing/src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import { Task } from './task';
+export const TASKS: Task[] = [
+  {
+    _id: 11,
+    title: 'Code like crazy',
+    completed: true,
+  },
+  {
+    _id: 23,
+    title: 'Become a Ninja',
+    completed: false
+  },
+  {
+    _id: 42,
+    title: 'Remember to Sleep',
+    completed: false,
+  },
+  {
+    _id: 32,
+    title: 'Be a Leader',
+    completed: true
+  }
+];
---
+++ MEAN/Angular/sample-app-routing/src/app/task.ts
@@ -0,0 +1,5 @@
+export class Task {
+    _id: number
+    title: string
+    completed: boolean
+}
