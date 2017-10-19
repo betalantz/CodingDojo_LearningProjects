cf0834364d2ae9def6aa20b0386e1a70afc492c8
betalantz <betalantz@gmail.com>
2017-10-19 14:19:48 -0700

    deleted notes-list component

--- MEAN/Integrated MEAN/anonymous_notes/client/src/app/app-routing.module.ts
+++ MEAN/Integrated MEAN/anonymous_notes/client/src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { NotesAddComponent } from './notes/notes-add/notes-add.component'
-import { NotesListComponent } from './notes/notes-list/notes-list.component'
 
 const routes: Routes = [
   { path: 'add', component: NotesAddComponent },
-  { path: 'show', component: NotesListComponent},
   { path: '**', pathMatch: 'full', redirectTo: '/add'}
 ];
 
