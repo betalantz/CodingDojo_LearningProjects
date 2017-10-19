09f9ba3a91d5185d984e31692f0058029463a90b
betalantz <betalantz@gmail.com>
2017-10-19 14:17:00 -0700

    moved list functions into add component

--- MEAN/Integrated MEAN/anonymous_notes/client/src/app/notes/notes-add/notes-add.component.ts
+++ MEAN/Integrated MEAN/anonymous_notes/client/src/app/notes/notes-add/notes-add.component.ts
@@ -12,16 +12,23 @@ import { NoteService } from './../note.service'
 })
 export class NotesAddComponent implements OnInit {
   new_note: Note
+  notes: Array<Note>
 
   constructor(private note_service: NoteService, private router: Router) { }
 
   ngOnInit() {
     this.new_note = new Note
 
+    this.note_service.get_all()
+    .then(notes => this.notes = notes)
+    .catch(err => console.log('get all notes err', err))
+
   }
   create_note(formController) {
     this.note_service.create(this.new_note)
-      .then(() => console.log('created'))
+      .then(() => this.note_service.get_all()
+          .then(notes => this.notes = notes)
+          .catch(err => console.log('get all notes refresh err', err)))
       .catch(err => console.log('note create error', err))
     formController.form.reset()
   }
