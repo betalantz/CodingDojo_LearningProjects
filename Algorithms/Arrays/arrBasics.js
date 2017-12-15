ed01e31a596c1a424b686a49134f0c1751a90395
betalantz <betalantz@gmail.com>
2017-12-14 16:30:28 -0800

    completed Arrays through removeDuplicates()

--- Algorithms/Arrays/arrBasics.js
+++ Algorithms/Arrays/arrBasics.js
@@ -39,3 +39,31 @@ console.log(pushFront(myArr,12));
 console.log(insertAt(myArr,2,100));
 console.log(popFront(myArr));
 console.log(removeAt(myArr,3));
+
+function swapPairs(arr){
+    let temp
+    for (let i=0; i<arr.length; i++){
+        if ((i+1)%2!=0){
+            temp = arr[i]
+        } else {
+            arr[i-1]=arr[i]
+            arr[i]=temp
+        }
+    }
+    return arr
+}
+let arr1 = [1,2,3,4]
+let arr2 = [true,'Nathan',42]
+console.log(swapPairs(arr1));
+console.log(swapPairs(arr2));
+
+function removeDuplicates(arr){
+    for(let k=0; k<arr.length; k++){
+        if (arr[k]==arr[k+1]){
+            removeAt(arr,k)
+        }
+    }
+    return arr
+}
+let arr3 = [4,5,6,7,7,8,9,9,10]
+console.log(removeDuplicates(arr3));
