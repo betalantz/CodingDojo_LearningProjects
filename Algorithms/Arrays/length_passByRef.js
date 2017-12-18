c94a5e954e4fefb40a28420de98f8d9a3ebb5c1a
betalantz <betalantz@gmail.com>
2017-12-18 12:54:04 -0800

    minToFront functioning

--- Algorithms/Arrays/length_passByRef.js
+++ Algorithms/Arrays/length_passByRef.js
@@ -1,3 +1,23 @@
 function minToFront(arr){
+    let min = arr[0]
+    let idx = 0
+    for(let j=1; j<arr.length; j++){
+        if (arr[j]<min) {
+            min=arr[j]
+            idx=j
+        }
+    }
+    for(let k=idx; k>0; k--){
+        arr[k]=arr[k-1]
+    }
+    arr[0]=min
+    return arr
+}
+let arr1 = [4,2,1,3,5]
+let arr2 = [2,8,4,9,3]
+console.log(minToFront(arr1));
+console.log(minToFront(arr2));
+
+function reverseArr(arr){
     
 }
