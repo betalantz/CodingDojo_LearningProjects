ed11687843f2f78560f14c57b5725dfd27870b80
betalantz <betalantz@gmail.com>
2018-02-18 11:21:37 -0800

    second commit

--- Algorithms/Arrays/quicksort.js
+++ Algorithms/Arrays/quicksort.js
@@ -5,37 +5,37 @@ function quickSort(arr, left, right){
  
  
    if(left < right){
-     pivot = right;
-     partitionIndex = partition(arr, pivot, left, right);
+        pivot = right;
+        partitionIndex = partition(arr, pivot, left, right);
      
-    //sort left and right
-    quickSort(arr, left, partitionIndex - 1);
-    quickSort(arr, partitionIndex + 1, right);
+        //sort left and right
+        quickSort(arr, left, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, right);
    }
    return arr;
- }
+}
 
  function partition(arr, pivot, left, right){
     var pivotValue = arr[pivot],
         partitionIndex = left;
  
     for(var i = left; i < right; i++){
-     if(arr[i] < pivotValue){
-       swap(arr, i, partitionIndex);
-       partitionIndex++;
-     }
+        if(arr[i] < pivotValue){
+            swap(arr, i, partitionIndex);
+            partitionIndex++;
+        }
    }
    swap(arr, right, partitionIndex);
    return partitionIndex;
- }
+}
 
 function swap(arr, i, j){
     var temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
- }
+}
         
- quickSort([11,8,14,3,6,2,7],0,6); 
+quickSort([11,8,14,3,6,2,7],0,6); 
 //[2, 3, 6, 7, 8, 11, 14]
 quickSort([11,8,14,3,6,2,1, 7],0,7);
 //[1, 2, 3, 6, 7, 8, 11, 14]
