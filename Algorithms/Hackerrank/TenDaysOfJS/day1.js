6711079e6075d27e69f184334305a480cce3f479
Lantz Warrick <betalantz@gmail.com>
2019-02-04 11:52:06 -0800

    Added arithmetic operators

---
+++ Algorithms/Hackerrank/TenDaysOfJS/day1.js
@@ -0,0 +1,29 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    area = length * width    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = (2*length)+(2*width)
+    return perimeter;
+}
+console.log(getArea(2, 3));
+console.log(getPerimeter(2, 3));
