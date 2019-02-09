e97dc312e7593b78857d4d3977b38e2da307cdfa
Lantz Warrick <betalantz@gmail.com>
2019-02-08 18:12:46 -0800

    Added chained if-else statements

--- Algorithms/Hackerrank/TenDaysOfJS/day2.js
+++ Algorithms/Hackerrank/TenDaysOfJS/day2.js
@@ -17,4 +17,26 @@ function vowelsAndConsonants(s) {
 const string1 = 'javascriptloops'
 vowelsAndConsonants(string1)
 
-
+function getGrade(score) {
+    let grade;
+    if (25 < score && score <= 30){
+        grade = 'A'
+    } 
+    else if (20 < score && score <= 25){
+        grade = 'B'
+    }
+    else if (15 < score && score <= 20) {
+        grade = 'C'
+    }
+    else if (10 < score && score <= 15) {
+        grade = 'D'
+    }
+    else if (5 < score && score <= 10){
+        grade = 'E'
+    }
+    else {
+        grade = 'F'
+    }
+    return grade;
+}
+console.log(getGrade(11));
