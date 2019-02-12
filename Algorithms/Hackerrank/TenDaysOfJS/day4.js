9643b47db3e105e8ba8b82538075882b7355e57c
Lantz Warrick <betalantz@gmail.com>
2019-02-11 17:55:17 -0800

    Implemented a Polygon class

--- Algorithms/Hackerrank/TenDaysOfJS/day4.js
+++ Algorithms/Hackerrank/TenDaysOfJS/day4.js
@@ -57,3 +57,18 @@ const obj0 = [
 ]
 console.log(getCount(obj0));
    
+class Polygon {
+    constructor(sides){
+        this.sides = sides
+    }
+    perimeter(){
+        return this.sides.reduce((acc, curr) => acc + curr)
+    }
+}
+const rectangle2 = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle2.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
