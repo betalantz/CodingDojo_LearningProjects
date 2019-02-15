957690d6baeb8d81a66cf75d66b722264e5489e8
Lantz Warrick <betalantz@gmail.com>
2019-02-14 16:34:12 -0800

    A solution with bitwise operators

--- Algorithms/Hackerrank/TenDaysOfJS/day6.js
+++ Algorithms/Hackerrank/TenDaysOfJS/day6.js
@@ -12,3 +12,16 @@ console.log(getDayName(dates[0]));
 console.log(getDayName(dates[1]));
 console.log(getDayName2(dates[0]));
 console.log(getDayName2(dates[1]));
+
+function getMaxLessThanK(n, k) {
+    let max = 0
+    for (let a = 1; a < n; a++) {
+        for (let b=a+1; b<=n; b++) {
+            let r = a & b
+            if (r < k && r > max){
+                max = r
+            }
+        }
+    }
+    return max
+}
