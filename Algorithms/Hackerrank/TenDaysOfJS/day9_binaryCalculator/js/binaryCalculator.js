3355ecbd817428a4c5f3be7d41018e27fa81484d
Lantz Warrick <betalantz@gmail.com>
2019-02-16 16:22:40 -0800

    Added programatic gen of document elements; added css

--- Algorithms/Hackerrank/TenDaysOfJS/day9_binaryCalculator/js/binaryCalculator.js
+++ Algorithms/Hackerrank/TenDaysOfJS/day9_binaryCalculator/js/binaryCalculator.js
@@ -1,8 +1,28 @@
 const display = document.createElement('div')
 display.id = 'res'
-const buttons = document.createElement('div')
-buttons.id = 'btns'
+const buttonsContainer = document.createElement('div')
+buttonsContainer.id = 'btns'
 const operators = ['+', '-', '*', '-']
 const row1 = ['0', '1', 'C', '=']
-const content = [...row1, ...operators]
+const buttons = [...row1, ...operators]
 const ids = ['0', '1', 'Clr', 'Eql', 'Sum', 'Sub', 'Mul', 'Div']
+for (let i = 0; i < 8; i++){
+    buttons[i] = document.createElement('button')
+    buttons[i].id = 'btn' + ids[i]
+    buttons[i].innerHTML = buttons[i]
+    switch(i) {
+        case 0:
+        case 1:
+            buttons[i].className = 'digits'
+            break
+        case 2:
+        case 3:
+            buttons[i].className = 'clr-eql'
+            break
+        default:
+            buttons[i].className = 'operators'
+    }
+    buttonsContainer.appendChild(buttons[i])
+}
+document.body.appendChild(display)
+document.body.appendChild(buttonsContainer)
