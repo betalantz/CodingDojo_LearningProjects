df231098b5aa6c87c71297ec6c393f71b0154d6b
Lantz Warrick <betalantz@gmail.com>
2019-03-05 20:08:51 -0800

    Implemented solution to CountingValleys

--- Algorithms/Hackerrank/countingValleys.js
+++ Algorithms/Hackerrank/countingValleys.js
@@ -19,3 +19,20 @@ console.log(countingValleys(hike1));
 console.log(countingValleys(hike2));
 console.log(countingValleys(hike3));
 console.log(countingValleys(hike4));
+console.log(countingValleysRefactored(hike1));
+console.log(countingValleysRefactored(hike2));
+console.log(countingValleysRefactored(hike3));
+console.log(countingValleysRefactored(hike4));
+
+// can eliminate the 'wasInValley' flag
+function countingValleysRefactored(s) {
+    let valleys = 0
+    let altitude = 0
+    for (const step of s) {
+        altitude = step == 'D' ? altitude-1 : altitude+1
+        if (altitude == 0 && step == 'U') {
+            valleys += 1
+        }
+    }
+    return valleys
+}
