e5132e72034e68a217769bd735f99b73080059db
Lantz Warrick <betalantz@gmail.com>
2019-03-07 15:58:56 -0800

    Implemented refactored RepeatedString

--- Algorithms/Hackerrank/repeatedString.js
+++ Algorithms/Hackerrank/repeatedString.js
@@ -5,7 +5,6 @@ function repeatedString(s, n) {
     for (let i = 0; i < repetitions; i++) {
         str += s
     }
-    console.log(str);
     for (let j = 0; j < n; j++) {
         if (str[j] == 'a') {
             a_count++
@@ -15,4 +14,23 @@ function repeatedString(s, n) {
 }
 const s1 = 'aba'
 const n1 = 10
+const s2 = 'a'
+const n2 = 10000
+const s3 = 'slicker'
+const n3 = 100000
 console.log(repeatedString(s1, n1));
+console.log(repeatedString(s2, n2));
+console.log(repeatedString(s3, n3));
+
+function repeatedStringRefactored(s, n) {
+    let res = []
+    for (let i = 0; i < n; i++) {
+        res.push(s[i%s.length]) 
+    }
+    const result = res.filter(char => char == 'a')
+    return result.length
+}
+
+console.log(repeatedStringRefactored(s1, n1));
+console.log(repeatedStringRefactored(s2, n2));
+console.log(repeatedStringRefactored(s3, n3));
