4188a0c1df42da3ce5d29f2bdeb92865ea2c6cd1
Lantz Warrick <betalantz@gmail.com>
2019-04-22 17:48:12 -0700

    Moved Algorithms dir to user root

--- Algorithms/insertIndex.js
+++
@@ -1,9 +0,0 @@
-function insertAt(arr, index, val){
-    var temp = arr[index];
-    arr[index]=val;
-    for (var i=index+1; i<arr.length); i++);
-        var temp2 = arr[i];    
-        arr[i] = temp;
-        arr[i+1] = temp2;
-}
-// iterate from back
--- Algorithms/reverse_arr.js
+++
@@ -1,9 +0,0 @@
-function reverseArr(arr){
-    var result = []
-    for (var i = arr.length-1; i >=0; i--){
-        result += arr[i];
-    }
-    console.log(result);
-    return result;
-}
-reverseArr([1,2,3])
